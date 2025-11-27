4eeb6eeffc08e64190e84426df19ce9b97bf181b
Wahyu Kwan <wahyukwan@gmail.com>
2025-11-28 02:28:43 +0700

    init

--- Bagirata/Item.swift
+++ Bagirata/Item.swift
@@ -2,17 +2,10 @@
 //  Item.swift
 //  Bagirata
 //
-//  Created by Moladin on 28/11/2025.
+//  Created by Wahyu K on 28/11/2025.
 //
 
 import Foundation
 import SwiftData
 
-@Model
-final class Item {
-    var timestamp: Date
-    
-    init(timestamp: Date) {
-        self.timestamp = timestamp
-    }
-}
+
